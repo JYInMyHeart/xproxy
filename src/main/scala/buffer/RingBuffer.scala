1576cfc1b4df64372d0d9c4fb529571c869c2fa2
alex <changhuxxu@gmail.com>
2019-08-01 18:33:19 +0800

    :recycle: reactor code by scala

--- src/main/scala/buffer/RingBuffer.scala
+++ src/main/scala/buffer/RingBuffer.scala
@@ -1,33 +1,3 @@
 package buffer
 
-import java.nio.ByteBuffer
-
-class RingBuffer(val byteBuffer: ByteBuffer,
-                 val capacity:Int){
-  private[this] var startPos:Int = _
-  private[this] var endPos:Int = _
-  private[this] var startAfterEnd:Boolean = _
-
-  def writeTo(): Unit ={
-
-  }
-
-  def storeLimit:Int = startAfterEnd match {
-    case true => startPos - endPos
-    case false => capacity - endPos
-  }
-
-  def retrieveLimit(sPos:Int,startAfterEnd:Boolean): Int = startAfterEnd match {
-    case true => capacity - endPos
-    case false => endPos - startPos
-  }
-
-  def retrieveLimit:Int = retrieveLimit(startPos,startAfterEnd)
-}
-
-object RingBuffer {
-  def apply(byteBuffer: ByteBuffer, capacity: Int): RingBuffer = new RingBuffer(byteBuffer, capacity)
-  def allocateDirect(cap:Int): RingBuffer = {
-    apply(ByteBuffer.allocateDirect(cap),cap)
-  }
-}
+class RingBuffer {}
